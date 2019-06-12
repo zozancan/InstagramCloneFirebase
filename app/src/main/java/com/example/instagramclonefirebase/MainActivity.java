6b9989887c44f62f2d9496f55ed5e88600e89748
zozancan <zzncan@gmail.com>
2019-06-12 11:25:39 +0300

    signIn and signUp process made.

--- app/src/main/java/com/example/instagramclonefirebase/MainActivity.java
+++ app/src/main/java/com/example/instagramclonefirebase/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.instagramclonefirebase;
 //SignInActivity
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText emailText;
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailText = findViewById(R.id.etMail);
+        passwordText = findViewById(R.id.etPassword);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+
+        }
+    }
+
+    public void signIn (View view) {
+
+        mAuth.signInWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public void signUp (View view) {
+
+        mAuth.createUserWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "User Created!", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
