79a28e54b86783c65ff79387d49e76b5200bfca0
zozancan <zzncan@gmail.com>
2019-06-12 13:48:02 +0300

    UploadActivity created.

--- app/src/main/java/com/example/instagramclonefirebase/UploadActivity.java
+++ app/src/main/java/com/example/instagramclonefirebase/UploadActivity.java
@@ -2,12 +2,29 @@ package com.example.instagramclonefirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class UploadActivity extends AppCompatActivity {
+
+    ImageView postImage;
+    EditText postCommentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        postImage = findViewById(R.id.ivPost);
+        postCommentText = findViewById(R.id.etPostComment);
+    }
+
+    public void upload(View view) {
+
+    }
+
+    public void imageSelect(View view) {
+        
     }
 }
